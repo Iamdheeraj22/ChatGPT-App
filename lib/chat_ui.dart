09bec2727e207dc57b1314a04373a7c806c28841
Dheeraj <dhiraj.prajapat@dotsquares.com>
2023-01-12 16:05:30 +0530

    Add depedency of Chat GPT in project

---
+++ lib/chat_ui.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ChatScreenUI extends StatefulWidget {
+  ChatScreenUI({super.key});
+
+  @override
+  State<ChatScreenUI> createState() => ChatScreenUIState();
+}
+
+class ChatScreenUIState extends State<ChatScreenUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
